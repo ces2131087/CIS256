d3fd71a02e7051dd3b65a76c042369c0fa22f7b0
unknown <CES2131087@maricopa.edu>
2025-03-29 12:53:06 -0700

    Updated Files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a simple GitHub repo with a basic .py file and image.
+
+this is an update to the README
--- simple_loop.py
+++ simple_loop.py
@@ -5,6 +5,8 @@ Part A
 
 This program is made to be be placed into a GitHub Repo
 """
+import math as math
+
 x = 'hi'
 b = 0
 
@@ -22,3 +24,8 @@ while x != 'stop':
     print()
 
 print(f'The loop iterated a total of {b} times')
+
+#update made to file:
+y = math.pi * b
+
+print(f'{b} multiplied by pi is {y:.2f} rounded to the nearest hundreth.')
